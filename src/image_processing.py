56e16b7ce37cbbce003513758999564095e9edde
Jose Mª García Quijada <73070970+Josemarii26@users.noreply.github.com>
2024-10-07 17:05:39 +0200

    calibration

--- src/image_processing.py
+++ src/image_processing.py
@@ -2,15 +2,29 @@ import cv2
 import numpy as np
 
 # Función que determina si la imagen está borrosa usando la varianza de Laplacian
-def es_borrosa(gris, umbral=5):
+def es_borrosa(gris, umbral=40):
     varianza = cv2.Laplacian(gris, cv2.CV_64F).var()
     return varianza < umbral
 
 # Función que determina si la imagen tiene franjas unicolor
-def tiene_franjas_unicolor(gris, tamaño_bloque=200, umbral=5):
+def tiene_franjas_unicolor(gris, tamaño_bloque=160, umbral=5):
+    # Tamaño de la imagen
     alto, ancho = gris.shape
-    return any(np.var(gris[y:min(y+tamaño_bloque, alto), x:min(x+tamaño_bloque, ancho)]) < umbral     # Evaluar la varianza en cada bloque, si algún bloque tiene varianza baja, se considera franja unicolor
-               for y in range(0, alto, tamaño_bloque) for x in range(0, ancho, tamaño_bloque))
+
+    # Iterar sobre bloques
+    for y in range(0, alto, tamaño_bloque):
+        for x in range(0, ancho, tamaño_bloque):
+            # Limitar los bloques al tamaño de la imagen
+            bloque = gris[y:min(y+tamaño_bloque, alto), x:min(x+tamaño_bloque, ancho)]
+            
+            # Calcula la varianza del bloque
+            varianza_bloque = np.var(bloque)
+
+            # Si la varianza del bloque es menor que el umbral, es una región unicolor
+            if varianza_bloque < umbral:
+                return True  # Se ha detectado una franja o región unicolor
+
+    return False  # No se detectó ninguna región unicolor
 
 # Función para evaluar la calidad de la imagen
 def evaluar_calidad(imagen):
